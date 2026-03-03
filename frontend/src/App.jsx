a6aa33a03672778c91bab7f2cf9ecab0b5bccd4e
Cindy <xwucindy@gmail.com>
2026-03-02 22:13:10 -0500

    voice_input in app.jsx, buggy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { AuthProvider, AuthContext } from './context/AuthContext';
 import ProtectedRoute from './context/ProtectedRoute';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import './App.css';
+import SpeechRecognition, {useSpeechRecognition} from "react-speech-recognition";
+import "regenerator-runtime/runtime";
+
+function VoiceRouter() {
+    const navigate = useNavigate();
+    const commands = [
+        {
+            command: ["go to *", "open *"],
+            callback: (page) => {
+                const path = `/${page.toLowerCase()}`;
+                navigate(path);
+            },
+        },
+    ];
+
+    const {transcript} = useSpeechRecognition({commands});
+
+    return (
+        <>
+            <p id = "transcript"> Transcript: {transcript}</p>
+            <button onClick={SpeechRecognition.startListening}> Voice Input </button>
+        </>
+    );
+
+}
+
 
 function App() {
+
+    //const [redirectURL, setRedirectURL] = useState("");
+
   return (
     <AuthProvider>
       <Router>
+          <VoiceRouter />
         <div className="app-container">
           <Routes>
             <Route path="/login" element={<Login />} />
@@ -27,6 +57,7 @@ function App() {
           </Routes>
         </div>  
       </Router>
+
     </AuthProvider>
   );
 }
---
+++ frontend/src/components/VoiceInput.jsx
@@ -0,0 +1,3 @@
+import React, { useState, useEffect, useRef } from "react";
+import SpeechRecognition, {useSpeechRecognition} from "react-speech-recognition";
+
